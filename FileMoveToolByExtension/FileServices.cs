ffb837531b3658d8bef49d1072dc4662c5d202d0
Justin Holcomb <jholcomb@retinalscreenings.com>
2021-11-17 14:38:10 -0600

    Added AppSettings.Json that allows custom extensions.

---
+++ FileMoveToolByExtension/FileServices.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileMoveToolByExtension
+{
+    public class FileServices
+    {
+        public string Designate_Directory()
+        {
+            string directory_source = "";
+            string directory_target = "";
+
+            Console.WriteLine("Enter directory to backup and search files");
+            directory_source = Console.ReadLine();
+
+            return directory_source;
+        }
+
+        public string Target_Directory()
+        {
+            string directory_target = "";
+
+            Console.WriteLine("Enter directory to move files");
+            directory_target = Console.ReadLine();
+
+            if (!Directory.Exists(directory_target))
+            {
+                Directory.CreateDirectory(directory_target);
+            }
+
+            return directory_target;
+        }
+
+
+        public async Task Directory_Backup(string directory_source)
+        {
+
+            ProcessStartInfo ps = new ProcessStartInfo();
+            ps.FileName = "cmd.exe";
+            ps.WindowStyle = ProcessWindowStyle.Hidden;
+            string directory_target = directory_source + @"\DataBackup";
+            ps.CreateNoWindow = true;
+            ps.Arguments = @"/k robocopy " + directory_source + " " + directory_target;
+
+            Process.Start(ps);
+            await Task.Delay(2000);
+            Console.WriteLine("Task Completed");
+        }
+
+        public List<string> FileSearch(string directory_source,string file_extension)
+        {
+            var userFiles = Directory.GetFiles(directory_source, file_extension, SearchOption.TopDirectoryOnly);
+            return userFiles.ToList();
+        }
+
+        public void CopyFiles(List<string> userFiles, string directory_target)
+        {
+            foreach (var file in userFiles)
+            {
+                var fileName = Path.GetFileName(file);
+                Console.WriteLine("Copying " + fileName + "...\n");
+                var newFileCreation = Path.Combine(directory_target, fileName);
+                Console.WriteLine("Copying to: " +  newFileCreation);
+
+                System.IO.File.Copy(file, newFileCreation, true);
+
+            }
+
+            
+        }
+
+
+    }
+
+}
+
--- FileMoveToolByExtension/Program.cs
+++ FileMoveToolByExtension/Program.cs
@@ -7,80 +7,30 @@ using System.Threading.Tasks;
 using System.Text;
 using Microsoft.Extensions.Configuration;
 
-namespace FileMoveToolByExtension // Note: actual namespace depends on the project name.
+namespace FileMoveToolByExtension
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
 
             var configuration = new ConfigurationBuilder()
                       .AddJsonFile("appsettings.json", false, true)
                       .Build();
 
-            string fileExtension = configuration["FileFormat"];
+            string file_extension = configuration["FileFormat"];
 
-            #region designate directories and make backup
+            FileServices FS = new FileServices();
 
-            Console.WriteLine("Choose Directory to backup");
-            var DirToBackup = Console.ReadLine();
-            Console.WriteLine("Creating back up folder on desktop");
-            //make back up folder on logged in user desktop
-            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string directory_source = FS.Designate_Directory();
+            string directory_target = FS.Target_Directory();
+            await FS.Directory_Backup(directory_source);
 
-            var backupFolderPath = desktopPath + @"\FileBackup";
+            var userFiles = FS.FileSearch(directory_source, file_extension);
+            FS.CopyFiles(userFiles, directory_target);
 
-            MakeBackUpDir(backupFolderPath, DirToBackup);
-
-            #endregion
-
-            #region Enter search path for files
-            // Backup created, now need to enter search path for files, and the directory to copy them
-            System.Threading.Thread.Sleep(3000);
-            Console.WriteLine("Enter path to search for files");
-            string RootPath = Console.ReadLine();
-            Console.WriteLine("Enter path to copy file");
-            string DestinationPath = Console.ReadLine();
-            Console.WriteLine("\n");
-            #endregion
-
-            //Function to search for files and copy them to new DIR
-            var userFiles = Directory.GetFiles(RootPath, fileExtension, SearchOption.AllDirectories);
-
-            Directory.CreateDirectory(DestinationPath);
-
-            foreach (var file in userFiles)
-            {
-                var fileName = Path.GetFileName(file);
-                Console.WriteLine(fileName);
-                Console.WriteLine("Copying...\n");
-                var newFileCreation = Path.Combine(DestinationPath, fileName);
-                Console.WriteLine(newFileCreation);
-
-                System.IO.File.Copy(file, newFileCreation, true);
-
-            }
-
-            var DestFiles = Directory.GetFiles(DestinationPath, fileExtension, SearchOption.AllDirectories);
-            foreach (var item in DestFiles)
-            {
-                var fileName = Path.GetFileName(item);
-                Console.WriteLine(fileName);
-            }
         }
-
-        #region Robocopy to create MIRROR directory for backup. 
-        public static void MakeBackUpDir(string backupDir, string DirToBackup)
-        {
-
-            ProcessStartInfo ps = new ProcessStartInfo();
-            ps.FileName = "cmd.exe";
-            ps.WindowStyle = ProcessWindowStyle.Hidden;
-            ps.CreateNoWindow = true;
-            ps.Arguments = @"/k robocopy " + DirToBackup + " " + backupDir + " /mir";
-            Process.Start(ps);
-        }
-        #endregion
-
+       
     }
+
 }
